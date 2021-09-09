545819be8ea259f935746eaaf25901fffe26c8f7
Edinaldo Nogueira <edinaldomoreiranog@gmail.com>
2021-09-09 10:05:52 -0300

    componentes estáticos básicos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import DateTable from "components/DateTable";
+import Footer from "components/Footer";
+import NavBar from "./components/NavBar";
 
 
 function App() {
   return (
+    <>
+      <NavBar/>
     <div></div>
-
+    <DateTable/>
+    <Footer/>
+  
+   </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+
+function Footer() {
+    return (
+        < footer  className = " footer mt-auto py-3 bg-dark " > 
+        < div  className = " container " > 
+          < p  className = " text-light " > App desenvolvido por < a  href = " https://github.com/EdinaldoMoreiraNogueira " target =" _blank " rel =" noreferrer " > Edinaldo Moreira Nogueira </ a > </ p > 
+          <p  className = "text-light " > < strong > Semana da Primavera Reagir </ strong > < br />
+            Evento promovido Pela escola DevNaldo: < a  href = "https://www.instagram.com/edm.nogueira/?hl=pt-br" target =" _blank " rel =" noreferrer " > @ edmoreira </ a >  </ p > 
+        </ div > 
+      </ footer >
+  
+    );
+  }
+  
+  export default Footer;
+  
