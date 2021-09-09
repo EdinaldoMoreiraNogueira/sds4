89aa28dbc834f3ebe29f145a979fa7aeea3a848e
Edinaldo Nogueira <edinaldomoreiranog@gmail.com>
2021-09-08 23:44:27 -0300

    Project create

---
+++ backend/src/main/java/com/devnaldo/devnaldovendas/DevnaldovendasApplication.java
@@ -0,0 +1,13 @@
+package com.devnaldo.devnaldovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevnaldovendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevnaldovendasApplication.class, args);
+	}
+
+}
